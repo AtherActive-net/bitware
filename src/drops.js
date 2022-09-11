4feccc4d2d61592071da540c35cf708a4caccd1f
AtherActive <ikbensalty@gmail.com>
2022-09-11 14:52:55 +0200

    fix: Adjusted timers / Added descriptions / Added who claimed

--- src/drops.js
+++ src/drops.js
@@ -1,13 +1,13 @@
 import Discord from "discord.js";
 
-const MIN_DROP_TIMER = 1; //10
-const MAX_DROP_TIMER = 2; // 30
-const MIN_DROP_DURATION = 1;
-const MAX_DROP_DURATION = 2;
+const MIN_DROP_TIMER = 180; // 180
+const MAX_DROP_TIMER = 500; // 500
+const MIN_DROP_DURATION = 30; //30
+const MAX_DROP_DURATION = 90; //90
 const MIN_BITS_DROPPED = 2;
 const MAX_BITS_DROPPED = 10;
 const CHIRP = "<:chirp:1015918908244508682>";
-export const RANDOM_DOUBLE_CHANCE = 2; //300, halves this value and uses that as value for the drop.
+export const RANDOM_DOUBLE_CHANCE = 300; //300, halves this value and uses that as value for the drop.
 
 export let droppedBits;
 export let remainingBits;
@@ -17,7 +17,7 @@ export let active = false;
 
 const DEFAULT_DESC = ` Chirps have been dropped! Type \`!claim\` to claim them!\n`
 const DESCRIPTIONS = [
-    `Did you know: Chirps are rare and only drop a few times per week!`,
+    `Did you know: Chirps drops can happen at any time?`,
     `Chirp drops expire quite quickly, so be sure to claim them as soon as possible!`,
     `There is a very small random chance to claim 2 chirps at once!`,
     'IM A BIRD, KAKAAA!',
@@ -25,6 +25,7 @@ const DESCRIPTIONS = [
     `The Chirp mint has no known location. The only known information is that it is high in a tree.`,
     `The Chirp production process is a closely guarded secret.`,
     `Chirp drops have a random duration. They can last anywhere from \`${MIN_DROP_TIMER}\` to \`${MAX_DROP_TIMER}\` minutes.`,
+    `Chirpware is currently at war with Chipware.`
 ]
 
 export function randomInRange(min, max) {
@@ -52,7 +53,7 @@ export async function timerComplete(framework,message) {
     await message.channel.send({embeds: [emb]});
 
     setTimeout(async () => {
-        await dropTimeout(framework,message)
+        if(dropHappening) await dropTimeout(framework,message);
     }, Math.floor(randomInRange(MIN_DROP_DURATION,MAX_DROP_DURATION) * 1000 * 60));
 }
 
@@ -65,7 +66,11 @@ export async function dropTimeout(framework,message) {
     const emb = new Discord.MessageEmbed()
         .setTitle("Drop has ended!");
     emb.setColor('#ebcf34');
-    emb.description = "The drop has ended! See you next time.";
+    emb.description = "The drop has ended! See you next time. These people claimed Chirps:\n";
+
+    claimedIds.forEach(element => {
+        emb.description += `<@${element}> `;
+    });
 
     await message.channel.send({embeds: [emb]});
     dropHappening = false;
